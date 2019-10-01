7e6a2ce5608f99fca65565eba14425dcda6012f0
PauloDS98 <desenvolver.paulo98@gmail.com>
2019-10-02 01:49:14 +0900

    js index

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,16 @@
 // This is a JavaScript file
 
+$(document).on("click", "#listarPizza", function(){
+
+  $(location).attr("href", "listar.html");
+});
+
+$(document).on("click", "#editarPizza", function(){
+
+  $(location).attr("href", "editar.html");
+})
+
+$(document).on("click", "#cancelarEditar", function(){
+
+  $(location).attr('href', 'listar.html');
+})
